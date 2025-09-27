22ca0e9fafe3c187da4d902ec952835b8e2f0ca3
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2025-09-27 09:59:50 +0100

    Added Cors

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,21 @@ import authRoutes from "./routes/authRoutes"
 import otpRoutes from "./routes/otpRoutes"
 import passRoutes from "./routes/passwordRoutes"
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 const port = config.get('port') as number
 const host = config.get('host') as string
-
+const corsOptions = {
+    origin: [
+        "https://quickbite-three-red.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    credentials: true
+}
 const app = express()
 
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
